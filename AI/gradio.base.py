c87c1b9fbc6269abedc2adf2b54d333947bd5da0
XiaoNnnnn <nike0918nike@gmail.com>
2025-06-02 13:42:35 +0800

    推APP.PY

--- AI/gradio.base.py
+++ AI/gradio.base.py
@@ -3,5 +3,5 @@ import gradio as gr
 def echo(message, history):
     return message
 
-demo = gr.ChatInterface(fn=echo, type="messages", examples=["hello", "hola", "merhaba"], title="Echo Bot")
+demo = gr.ChatInterface(fn=echo, type="messages", examples=["不死三振是甚麼?", "強迫進壘定義為何?", "高飛必死球是甚麼意思?"], title="九局通")
 demo.launch()
